5d57b2fc39aa46739e4077ca480e98fda4a140dc
mitch <bigmitchsystems@gmail.com>
2024-01-25 05:20:25 +0200

    added first notes and practice code

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,15 @@
+val pi = getPiValue()
+const val PI = 3.14
 fun main(){
-    print("hello world")
+    var country: String
+    var name = "mitch"
+    var surname = "severa"
+    val age = 28
+    var city = "harare"
+    country = "Zimbabwe"
+    println("hello world " + name + " " + surname + " at age $age")
+    println("my city is $city in " + country)
+    println("pi reference is " + pi)
 }
+
+fun getPiValue(): Double = PI
