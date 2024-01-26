738e128b97355093aa1a4fdd94307e5446bec71d
mitchell.severa <mitchell.severa@entelect.co.za>
2024-01-26 17:36:11 +0200

    added conditional statements if...else

--- src/Main.kt
+++ src/Main.kt
@@ -14,6 +14,20 @@ fun main(){
     println("surname length is " + surname.length)
     println(surname.uppercase())
     println(name.lowercase())
+
+    var a = 5
+    var b = 6
+
+    if (a < b) {
+        println("" + a + " is less than " + b)
+    } else if (a == b) {
+        println("the values are equal")
+    }else {
+        println("" + a + " is greater than " + b)
+    }
+
+    val result = if (a == b) "the values are equal" else "the values are not equals"
+    println(result)
 }
 
 fun getPiValue(): Double = PI
